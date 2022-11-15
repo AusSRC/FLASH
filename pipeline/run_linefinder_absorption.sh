ca77ee5b36f482463efa674ea627b4ff9bc304fe
Gordon German <gordon.german@csiro.au>
2022-11-15 11:33:51 +0800

    added master slurm script

--- pipeline/run_linefinder_absorption.sh
+++ pipeline/run_linefinder_absorption.sh
@@ -1,28 +1,23 @@
-# Set path to FLASH FINDER
-FINDER=$HOME/src/FLASH/linefinder 
-export FINDER
-
-# Set path to MULTINEST
-export MULTINEST=$HOME/src/FLASH/PyMultinest/MultiNest
-
-# Set path to PYMULTINEST
-export PYMULTINEST=$HOME/src/FLASH/PyMultinest
-
-# Add MultiNest library to dynamic library path
-export DYLD_LIBRARY_PATH=$MULTINEST/lib:${DYLD_LIBRARY_PATH}
-export LD_LIBRARY_PATH=$MULTINEST/lib:${LD_LIBRARY_PATH}
+#!/bin/bash
+#SBATCH --job-name=linefinder
+#SBATCH -N 1 # nodes
+#SBATCH -n 1 # tasks
+#SBATCH --cpus-per-task 28
+#SBATCH --mem=32G
 
 # Set path to Matplotlib set up
-export MATPLOTLIBRC=$HOME/.local/lib/python3.10/site-packages/matplotlib/
+#export MATPLOTLIBRC=$HOME/.local/lib/python3.10/site-packages/matplotlib/
+
+module load python/3.9.1
 
 /usr/bin/env python3 $FINDER/flash_finder_NOMPI.py \
 --x_units 'frequency' \
 --rest_frequency '1420.405752' \
 --y_units 'abs' \
---out_path $FINDER'/testing/chains' \
---data_path $FINDER'/testing/data/33616/spectra_ascii' \
---sourcelog 'sources1.log' \
---model_path $FINDER/'model.txt' \
+--out_path $1 \
+--data_path $2 \
+--sourcelog $3 \
+--model_path $4 \
 --nlive 1000 \
 --channel_function 'none' \
 --plot_restframe 'peak' \
--- pipeline/run_linefinder_inverted.sh
+++ pipeline/run_linefinder_inverted.sh
@@ -1,28 +1,16 @@
-# Set path to FLASH FINDER
-FINDER=$HOME/src/FLASH/linefinder 
-export FINDER
-
-# Set path to MULTINEST
-export MULTINEST=$HOME/src/FLASH/PyMultinest/MultiNest
-
-# Set path to PYMULTINEST
-export PYMULTINEST=$HOME/src/FLASH/PyMultinest
-
-# Add MultiNest library to dynamic library path
-export DYLD_LIBRARY_PATH=$MULTINEST/lib:${DYLD_LIBRARY_PATH}
-export LD_LIBRARY_PATH=$MULTINEST/lib:${LD_LIBRARY_PATH}
+#!/bin/bash
 
 # Set path to Matplotlib set up
-export MATPLOTLIBRC=$HOME/.local/lib/python3.10/site-packages/matplotlib/
+#export MATPLOTLIBRC=$HOME/.local/lib/python3.10/site-packages/matplotlib/
 
 /usr/bin/env python3 $FINDER/flash_finder_NOMPI.py \
 --x_units 'frequency' \
 --rest_frequency '1420.405752' \
 --y_units 'abs' \
---out_path $FINDER'/testing/chains' \
---data_path $FINDER'/testing/data/33616/spectra_ascii' \
---sourcelog 'sources1.log' \
---model_path $FINDER/'model.txt' \
+--out_path $1 \
+--data_path $2 \
+--sourcelog $3 \
+--model_path $4 \
 --nlive 1000 \
 --channel_function 'none' \
 --plot_restframe 'peak' \
