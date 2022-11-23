cf981b84fcdd0f7a919501a8452da02c13625b22
Gordon German <gordon.german@csiro.au>
2022-11-23 12:13:13 +0800

    Added option to override commandline args

--- pipeline/run_container.sh
+++ pipeline/run_container.sh
@@ -3,17 +3,17 @@
 sbatch <<EOT
 #!/bin/bash
 #SBATCH --job-name=flashfinder
-#SBATCH --output="$2"/outputs/logs/finder_output_%j.log
-#SBATCH --error="$2"/outputs/logs/finder_error_%j.log
+#SBATCH --output="$1"/outputs/logs/finder_output_%j.log
+#SBATCH --error="$1"/outputs/logs/finder_error_%j.log
 #SBATCH -N 1 # nodes
 #SBATCH -n 1 # tasks
 #SBATCH --cpus-per-task 28
 #SBATCH --mem=75G
 
 module load singularity/3.8.6
-mkdir -p "$2"/outputs/logs
-echo "Started with $1: $2, $3"
-singularity exec --bind "$2":/data,"$3":/config linefinder.sif /home/flash/FLASH/pipeline/run_linefinder_$1.sh
+mkdir -p "$1"/outputs/logs
+echo "Started with $1: $2"
+singularity exec --bind "$1":/data,"$2":/config linefinder.sif /home/flash/FLASH/pipeline/run_linefinder.sh
 
 exit 0
 EOT
--- pipeline/run_slurm_script.sh
+++ pipeline/run_slurm_script.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # pass to container script: 
-#   1) the linefinder script (absorption or inverted)
-#   2) input data directory
-#   3) linefinder config directory (holds sources.log and model .txt)
-jid1=$(/bin/bash ./run_container.sh absorption /scratch/ja3/ger063/flash/data4 /scratch/ja3/ger063/flash/config3)
-jid2=$(/bin/bash ./run_container.sh absorption /scratch/ja3/ger063/flash/data4 /scratch/ja3/ger063/flash/config4)
-jid3=$(/bin/bash ./run_container.sh absorption /scratch/ja3/ger063/flash/data4 /scratch/ja3/ger063/flash/config5)
+#   1) input data directory
+#   2) linefinder config directory (holds linefinder.ini, sources.log and model .txt)
+jid1=$(/bin/bash ./run_container.sh /scratch/ja3/ger063/flash/data4 /scratch/ja3/ger063/flash/config3)
+jid2=$(/bin/bash ./run_container.sh /scratch/ja3/ger063/flash/data4 /scratch/ja3/ger063/flash/config4)
+jid3=$(/bin/bash ./run_container.sh /scratch/ja3/ger063/flash/data4 /scratch/ja3/ger063/flash/config5)
 
 j1=$(echo $jid1 | awk '{print $4}')
 j2=$(echo $jid2 | awk '{print $4}')
