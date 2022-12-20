61c1999ab09c916f5e098d3cc59098c816fd1768
Gordon German <gordon.german@csiro.au>
2022-12-20 14:45:51 +0800

    updated comments

--- spectral_plot/config.py
+++ spectral_plot/config.py
@@ -16,20 +16,26 @@ hi_rest=1420.40575177
 c=2.99792458e5
 
 PLOT = True # Generate the plots - GWHG
-PEAKFLUX = 0.0 # Only generate the plots if the peak flux is over this value
+PEAKFLUX = 0.0 # Only generate the plots for components where the peak flux is over this value
 ASCII = True # Generate the ascii files for the linefinder - GWHG
 ARCHIVE = False # tar and push results to Acacia - GWHG
 DEBUG = False
 NUMCORES = 32 # Number of cores requested for multiprocessing - this will be limited to max available.
 
-# DATA PATH templates - absolute
-GlobTemplate = '/data/sourceSpectra/4*'
-CatalogueTemplate = '/data/sourceSpectra/catalogues/selavy-image.i*.SB%s.cont.*taylor.0.restored*.components.xml'
-SpecHduTemplate = '/data/sourceSpectra/%s/spec_*.fits'
-ContCubeTemplate = '/data/contcubes/%s/spectrum_contcube_SB%s_component_%s.txt'
-NoiseTemplate = '/data/noise/%s/noise_SB%s_component_%s.fits'
-OutputTemplate1 = '/data/sourceSpectra/%s/spectra_ascii/'
-OutputTemplate2 = '/data/sourceSpectra/%s/spectra_plots/'
+#################################### DATA PATH templates - absolute #################
+# "/data" is the data directory set in run_slurm_spectral.sh. You can then 
+# reflect any subdirectory structures here
+
+GlobTemplate = '/data/sourceSpectra/4*' # only used if the sbid value from "run_slurm_spectrals.sh" is "all"
+
+CatalogueTemplate = '/data/sourceSpectra/catalogues/selavy-image.i*.SB%s.cont.*taylor.0.restored*.components.xml' # subdirectory holding the image catalogues
+SpecHduTemplate = '/data/sourceSpectra/%s/spec_*.fits' # the component files for the given sbid
+ContCubeTemplate = '/data/contcubes/%s/spectrum_contcube_SB%s_component_%s.txt' # contcube spectra to calculate optical depth
+NoiseTemplate = '/data/noise/%s/noise_SB%s_component_%s.fits' # the noise cubes
+OutputTemplate1 = '/data/sourceSpectra/%s/spectra_ascii/' # where to create output ascii files
+OutputTemplate2 = '/data/sourceSpectra/%s/spectra_plots/' # where to create output plot png files
+
+## Leave these as they are unless you have good reason to change them
 AsciiTemplate1 = OutputTemplate1 + 'SB%s_component_%s_opd.dat'
 AsciiTemplate2 = OutputTemplate1 + 'SB%s_component_%s_flux.dat'
 PlotTemplate = OutputTemplate2 + 'SB%s_component_%s_opd.png'
@@ -44,7 +50,7 @@ localtarpath = os.getcwd() # where the tarball is on local drive - GWHG
 endpoint = "https://projects.pawsey.org.au"
 project = "ja3"
 bucket = "flash"
-certfile = "certs.json"
+certfile = "certs.json" # holds the objectstore id strings for the project and/or user
 storepath = 'pilot2_outputs' # where the tarball will be relative to the bucket - GWHG
 
 
