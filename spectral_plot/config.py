fd295b9fabc24d36008411354a1b7209a779c4c1
Gordon German <gordon.german@csiro.au>
2023-02-09 11:35:59 +0800

    Changed some of the templates and default peak flux value

--- spectral_plot/config.py
+++ spectral_plot/config.py
@@ -16,7 +16,7 @@ hi_rest=1420.40575177
 c=2.99792458e5
 
 PLOT = True # Generate the plots - GWHG
-PEAKFLUX = 0.0 # Only generate the plots for components where the peak flux is over this value
+PEAKFLUX = 0.045 # Only generate the plots for components where the peak flux is over this value
 ASCII = True # Generate the ascii files for the linefinder - GWHG
 ARCHIVE = False # tar and push results to Acacia - GWHG
 DEBUG = False
@@ -28,22 +28,22 @@ NUMCORES = 32 # Number of cores requested for multiprocessing - this will be lim
 
 GlobTemplate = '/data/sourceSpectra/4*' # only used if the sbid value from "run_slurm_spectrals.sh" is "all"
 
-CatalogueTemplate = '/data/sourceSpectra/catalogues/selavy-image.i*.SB%s.cont.*taylor.0.restored*.components.xml' # subdirectory holding the image catalogues
-SpecHduTemplate = '/data/sourceSpectra/%s/spec_*.fits' # the component files for the given sbid
-ContCubeTemplate = '/data/contcubes/%s/spectrum_contcube_SB%s_component_%s.txt' # contcube spectra to calculate optical depth
-NoiseTemplate = '/data/sourceSpectra/noise/%s/noise_SB%s_component_%s.fits' # the noise cubes
-OutputTemplate1 = '/data/sourceSpectra/%s/spectra_ascii/' # where to create output ascii files
-OutputTemplate2 = '/data/sourceSpectra/%s/spectra_plots/' # where to create output plot png files
+CatalogueTemplate = '/data/sourceSpectra/catalogues/selavy-image.i*.SB%s.cont.*taylor.0.restored*.components.xml' # subdirectory holding the image catalogues: substitute % (sbid)
+SpecHduTemplate = '/data/sourceSpectra/%s/spec_*.fits' # the component files for the given sbid: substitute % (sbid)
+ContCubeTemplate = '/data/contcubes/%s/spectrum_contcube_SB%s_component_%s.txt' # contcube spectra to calculate optical depth: substitute % (sbid,sbid,compno)
+NoiseTemplate = '/data/sourceSpectra/noise/%s/noise_SB%s_component_%s.fits' # the noise cubes: substitute % (sbid,sbid,compno)
+OutputTemplate1 = '/data/sourceSpectra/%s/spectra_ascii/' # where to create output ascii files: substitute % (sbid)
+OutputTemplate2 = '/data/sourceSpectra/%s/spectra_plots/' # where to create output plot png files: substitute % (sbid)
 
 ## Leave these as they are unless you have good reason to change them
-AsciiTemplate1 = OutputTemplate1 + 'SB%s_component_%s_opd.dat'
-AsciiTemplate2 = OutputTemplate1 + 'SB%s_component_%s_flux.dat'
-PlotTemplate = OutputTemplate2 + 'SB%s_component_%s_opd.png'
-PlotTemplate2 = OutputTemplate2 + 'SB%s_component_%s_flux.png'
+AsciiTemplate1 = OutputTemplate1 + 'SB%s_component_%s_opd.dat' #: substitute % (sbid,compno)
+AsciiTemplate2 = OutputTemplate1 + 'SB%s_component_%s_flux.dat' #: substitute % (sbid,compno)
+PlotTemplate = OutputTemplate2 + 'SB%s_component_%s_opd.png' #: substitute % (sbid,compno)
+PlotTemplate2 = OutputTemplate2 + 'SB%s_component_%s_flux.png' #: substitute % (sbid,compno)
 
 # Variables for optional tarring of output results
-TARPATH = '/data/sourceSpectra/%s' # 'root' directory for tarring operation - GWHG
-TARNAME = 'SB%s_output_plots_and_ascii.tar.gz'   # Template for tarball name - GWHG
+TARPATH = '/data/sourceSpectra/%s' # 'root' directory for tarring operation - GWHG: substitute % (sbid)
+TARNAME = 'SB%s_output_plots_and_ascii.tar.gz'   # Template for tarball name - GWHG: substitute % (sbid)
 
 # Variables for optional storage to Acacia objectstore
 localtarpath = os.getcwd() # where the tarball is on local drive - GWHG
