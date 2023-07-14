71d5deb2a06256bc85ccdc52f6252bfe08af1956
Gordon German <gordon.german@csiro.au>
2023-07-14 13:02:21 +0800

    Added query of sbid for metadata

--- database/get_png_from_db.py
+++ database/get_png_from_db.py
@@ -38,6 +38,9 @@ def get_cursor(conn):
     cursor = conn.cursor()
     return cursor
 
+##################################################################################################
+##################################################################################################
+
 def get_max_sbid_version(cur,sbid_num,version=None):
 
     # If version=None, returns the sbid_id:version for the latest version number of the sbid_num in the SBID table
@@ -47,7 +50,11 @@ def get_max_sbid_version(cur,sbid_num,version=None):
     if version:
         query = "select id from sbid where sbid_num = %s and version = %s;"
         cur.execute(query,(sbid_num,version))
-        sbid_id = int(cur.fetchall()[0][0])
+        try:
+            sbid_id = int(cur.fetchall()[0][0])
+        except IndexError:
+            # sbid doesn't exist
+            sbid_id = None
     else:
         query = "select id,version from sbid where sbid_num = %s and version = (select max(version) from sbid where sbid_num = %s);"
         cur.execute(query,(sbid_num,sbid_num))
@@ -58,6 +65,7 @@ def get_max_sbid_version(cur,sbid_num,version=None):
             sbid_id = None
             version = 0
     return sbid_id,version
+##################################################################################################
 
 
 def returnBrightestSources(names,number=None):
@@ -87,38 +95,33 @@ def returnBrightestSources(names,number=None):
         for source in sources:
             bright_sources.append(source)
     return bright_sources,idx
-
-def usage():
-    print()
-    print("USAGE:")
-    print("python3 get_png_from_db.py <directory to download to> <sbid> <top n brightest components>")
-    print("     eg python3 get_png_from_db.py /home/ger063/tmp 43426 20")
-    print()
-    print("     will download brightest 20 sources from sbid 43426")
-    print()
-    print("     python3 get_png_from_db.py /home/ger063/tmp 43426:2 20")
-    print("     will download the brightest 20 sources from version 2 of sbid 43426 in the db")
-
-    print("     -- add 'flux' to download the flux images. Default is opd images only")
-    sys.exit()
-
-##################################################################################################
 ##################################################################################################
 
-if __name__ == "__main__":
+def query_db_for_sbid(cur,sbid):
 
-    conn = connect()
-    cur = get_cursor(conn)
+    query = "select sbid_num, version, spect_runid, id, detectionF from sbid where sbid_num = %s"
+    cur.execute(query,(sbid,))
+    res = cur.fetchall()
+    print("DATE\t\t\tSBID\tVERSION\tID\tTAG\t\t\tLINEFINDER RUN")
+    for result in res:
+        # get the run tags
+        spect_q = "select run_tag, date from spect_run where id = %s"
+        cur.execute(spect_q,(result[2],))
+        spect_tag,date = cur.fetchall()[0]
+        print(f"{date}\t{result[0]}\t{result[1]}\t{result[3]}\t{spect_tag}\t\t\t{result[4]}")
+    return
 
-    if len(sys.argv) < 4:
-        usage()
+##################################################################################################
+
+def get_plots_for_sbid(cur,args):
+
     # The directory for downloads:
-    dir_download = sys.argv[1]
+    dir_download = args[1]
 
     # The sbid you want to use - if a version is not declared ("45833" rather than "45833:2"),
     # then use the latest version
     version = None
-    sbid_str = sys.argv[2]
+    sbid_str = args[2]
     if ":" in sbid_str:
         sbid = int(sbid_str.split(":")[0])
         version = int(sbid_str.split(":")[1])
@@ -132,16 +135,19 @@ if __name__ == "__main__":
     # download the top 20 components (and their 'a', 'b', 'c' etc varieties, so there will
     # be more than 20 files!)
     #
-    num_sources = int(sys.argv[3])
+    num_sources = int(args[3])
     try:
         # The image type to download ("opd" or "flux")
-        img_type = sys.argv[4]
+        img_type = args[4]
     except IndexError:
         img_type = 'opd'
 
     if not sid:
+        if version == 0:
+            version = "any_version"
         print(f"sbid {sbid}:{version} is not in the database!")
-        sys.exit()
+        return
+
     print(f"For sbid = {sbid}:{version}")
 
     # Get the components in the database for the selected sbid:
@@ -164,3 +170,52 @@ if __name__ == "__main__":
         name = source.replace(".fits",f"_{img_type}.png")
         open(f"{dir_download}/{name}", 'wb').write(data[0])
     print(f"Downloaded {len(sources)} files from sbid {sbid}")
+
+    return
+##################################################################################################
+
+def usage():
+    print()
+    print("USAGE:")
+    print("python3 get_png_from_db.py <directory to download to> <sbid> <top n brightest components>")
+    print("     eg python3 get_png_from_db.py /home/ger063/tmp 43426 20")
+    print()
+    print("     will download brightest 20 sources from sbid 43426")
+    print()
+    print("     python3 get_png_from_db.py /home/ger063/tmp 43426:2 20")
+    print("     will download the brightest 20 sources from version 2 of sbid 43426 in the db")
+
+    print("     -- add 'flux' to download the flux images. Default is opd images only")
+    print()
+    print("ALTERNATE USAGE - query db for sbid metatdata")
+    print("     python3 get_png_from_db.py 45833")
+    print()
+    print("     will return metadata on sbid, eg number of versions, tags etc")
+    sys.exit()
+
+##################################################################################################
+##################################################################################################
+
+if __name__ == "__main__":
+
+    if len(sys.argv) not in [2,4,5]:
+        usage()
+
+    conn = connect()
+    cur = get_cursor(conn)
+    
+    # Query db for sbid metadata
+    if len(sys.argv) == 2:
+        try:
+            sbid = int(sys.argv[1])
+        except:
+            usage()
+        query_db_for_sbid(cur,sbid)
+
+    # Get plots for sbid
+    elif len(sys.argv) > 3:
+        get_plots_for_sbid(cur,sys.argv)
+
+    else:
+        usage()
+
