308240d3125b4a75e74c489d587f563344ce6259
Gordon German <gordon.german@csiro.au>
2024-07-15 10:36:43 +0800

    Removed cron jobs, added 300s timeout

--- django/flashdb/service/flashdb.sh
+++ django/flashdb/service/flashdb.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-cd /home/ubuntu/django/flashdb
+# user's home directory
+FHOME=/home/flash
+
+cd $FHOME/django/flashdb
 
 # Production ssl server using uWSGI: Use route and sed to determine ip address of external-facing interface:
-sudo uwsgi --https `ip -o route get to 150.229.69.37 | sed -n 's/.*src \([0-9.]\+\).*/\1/p' `:443,/etc/letsencrypt/live/flash.aussrc.org/fullchain.pem,/etc/letsencrypt/live/flash.aussrc.org/privkey.pem service/flashdb.ini >> service/flashgui.log 2>&1
+sudo uwsgi --https `ip -o route get to 150.229.69.37 | sed -n 's/.*src \([0-9.]\+\).*/\1/p' `:443,$FHOME/django/flashdb/certs/fullchain.pem,$FHOME/django/flashdb/certs/privkey.pem service/flashdb.ini >> service/flashgui.log 2>&1
