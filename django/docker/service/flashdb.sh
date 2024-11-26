157dedbc9824fd41c286bf1930cedcc742d63898
Tushar <tushar.patel@aussrc.org>
2024-11-26 10:47:31 +0000

    update dockerfile

--- django/docker/service/flashdb.sh
+++ django/docker/service/flashdb.sh
@@ -2,9 +2,7 @@
 
 # user's home directory
 FHOME=/home/flash
-cd $FHOME/django/flashdb
+cd $FHOME/django/flashdb || exit 1
 
-# Production ssl server using uWSGI: Use route and sed to determine ip address of external-facing interface:
-sudo uwsgi --https `ip -o route get to 150.229.69.37 | sed -n 's/.*src \([0-9.]\+\).*/\1/p' `:443 service/flashdb.ini >> service/flashgui.log 2>&1
-
-
+# Production server using uWSGI: Use route and sed to determine ip address of external-facing interface:
+sudo uwsgi --http `ip -o route get to 150.229.69.37 | sed -n 's/.*src \([0-9.]\+\).*/\1/p'`:8000 service/flashdb.ini
