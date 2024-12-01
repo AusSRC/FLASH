c13ce568e11cb32559dfadda429722eeb50c0b34
Tushar <tushar.patel@aussrc.org>
2024-12-01 22:05:02 +0000

    add d

---
+++ django/db/init.sql
@@ -0,0 +1,29 @@
+-- Enable dblink extension
+CREATE EXTENSION IF NOT EXISTS dblink;
+
+-- Create admin user if it doesn't exist
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'admin') THEN
+        CREATE USER "admin" WITH PASSWORD 'F04nmbvD2!f4kjvC';
+        ALTER USER "admin" WITH SUPERUSER;
+    END IF;
+END $$;
+
+-- Create flash user if it doesn't exist
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'flash') THEN
+        CREATE USER "flash" WITH PASSWORD 'password';
+        ALTER USER "flash" WITH SUPERUSER;
+    END IF;
+END $$;
+
+-- Create chad database if it doesn't exist
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'flashdb') THEN
+        PERFORM dblink_exec('dbname=postgres', 'CREATE DATABASE flashdb WITH TEMPLATE = template0 ENCODING = ''UTF8'' LC_COLLATE = ''en_US.utf8'' LC_CTYPE = ''en_US.utf8''');
+        PERFORM dblink_exec('dbname=postgres', 'ALTER DATABASE flashdb OWNER TO "admin"');
+    END IF;
+END $$;
