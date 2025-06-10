904690df40144cdae1e108b53a42fe56b43af9d3
Gordon German <gordon.german@csiro.au>
2025-06-10 14:27:01 +0800

    Corrected array syntax

--- pipeline/cronjobs/tar_detection_outputs.sh
+++ pipeline/cronjobs/tar_detection_outputs.sh
@@ -8,4 +8,5 @@ SBIDARRAY=( "$@" )
 for SBID1 in "${SBIDARRAY[@]}"; do
     echo "Tarring $SBID1 linefinder results"
     cd $DATA/$SBID1/outputs; tar -zcvf linefinder.tar.gz results* *stats.dat *.pdf; mv linefinder.tar.gz ../
+done
 exit 0
