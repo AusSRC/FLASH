a9c504299d7edbc8dd5ba4a375f81c141e9d9a19
flash <gordon.german@csiro.au>
2025-06-23 04:14:39 +0000

    Modified for Oracle

--- django/flashdb/service/flashdb.sh
+++ django/flashdb/service/flashdb.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # user's home directory
-FHOME=/home/flash
+FHOME=/home/flash/src/FLASH
 
 cd $FHOME/django/flashdb
 
 # Production ssl server using uWSGI: Use route and sed to determine ip address of external-facing interface:
-sudo uwsgi --https `ip -o route get to $IPCHECK | sed -n 's/.*src \([0-9.]\+\).*/\1/p' `:443,$FHOME/django/flashdb/certs/fullchain.pem,$FHOME/django/flashdb/certs/privkey.pem service/flashdb.ini >> service/flashgui.log 2>&1
+#sudo uwsgi --https `ip -o route get to $IPCHECK | sed -n 's/.*src \([0-9.]\+\).*/\1/p' `:443,$FHOME/django/flashdb/certs/fullchain.pem,$FHOME/django/flashdb/certs/privkey.pem service/flashdb.ini >> service/flashgui.log 2>&1
+sudo uwsgi --https :443,$FHOME/django/flashdb/certs/fullchain.pem,$FHOME/django/flashdb/certs/privkey.pem service/flashdb.ini >> service/flashgui.log 2>&1
