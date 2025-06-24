1edec3489f59978cc2184aced062f143084434a6
Gordon German <gordon.german@csiro.au>
2025-06-24 10:22:16 +0800

    removed check for files; now done on client

--- pipeline/cronjobs/hpc_platform/detection_processing.sh
+++ pipeline/cronjobs/hpc_platform/detection_processing.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
+######################################################################################################
+# Author: GWHG, AusSRC
+#
+# Date: 20/06/2025
+#
+# This script runs in response to a cron script running on a client machine (currently at Oracle).
+# It checks a local log (put here by the client) for any sbids that require processing.
+# If there are any, it retrieves the ASCII input files from the client (via scp) and processes them.
+#
+# Note that after retrieval, it will delete the files on the client.
+#
+# It then tars up the outputs, pushes them to the client and initiates a script on the client to
+# upload the outputs to the FLASH database (via ssh).
+
 #######################################################################################################
 # Edit these for the specific user:
 CASDA_EMAIL="user@email"
 CASDA_PWD="password_at_CASDA"
 ORACLE_DB="TRUE"
+CLIENT="152.67.97.254"
 #######################################################################################################
 
 FLASHPASS=$1
 MODE=$2
 
 output=""
-if [ $ORACLE_DB = "TRUE" ]; then
-    HOST = "152.67.97.254"
-fi
 source ~/setonix_set_local_env.sh
 # Local directories on setonix:
 DBDIR="/home/$USER/src/database/"
@@ -28,15 +40,11 @@ PARENT_DIR=$DATA
 # Directory to move bad data files to:
 BAD_FILES_DIR="$DATA/bad_ascii_files/"
 
-
-# Query FLASHDB for new sbids
+cd $CRONJOBS
+# Check logs for new sbids to process
 if [ "$MODE" = "INVERT" ]; then
-    ssh -i $FLASHDB/oracle_flash_vm.key flash@$HOST "cd ~/src/FLASH/database; python3.9 ./db_utils.py -m SBIDSTODETECT -i True -pw $FLASHPASS > ../cronjobs/find_invert_detection.log"
-    scp -i $FLASHDB/oracle_flash_vm.key flash@$HOST:/home/flash/src/cronjobs/find_invert_detection.log .
-    output=$( tail -n 1 find_detection.log)
+    output=$( tail -n 1 find_invert_detection.log)
 else
-     ssh -i $FLASHDB/oracle_flash_vm.key flash@$HOST "cd ~/src/FLASH/database; python3.9 $FLASHDB/db_utils.py -m SBIDSTODETECT -pw $FLASHPASS > ../cronjobs/find_detection.log"
-    scp -i $FLASHDB/oracle_flash_vm.key flash@$HOST:/home/flash/src/cronjobs/find_detection.log .
     output=$( tail -n 1 find_detection.log)
 fi
 sbids=${output:1: -1}
@@ -56,7 +64,7 @@ SBIDARRAY=()
 SBIDARRAY=${SBIDARR[@]:0:10}
 echo -e "\nprocessing ${SBIDARRAY[@]}"
 
-# Get the data for the sbids from the FLASHDB and process
+# Get the data for the sbids from the client and process
 
 # Initialise status file
 cd $DETECTDIR
@@ -67,35 +75,22 @@ for SBID1 in ${SBIDARRAY[@]}; do
     PARENT1="$PARENT_DIR/$SBID1"
     DIR1="$PARENT1/spectra_ascii"
     mkdir -p "$PARENT1/config"
-    # Check if we need to download the data:
-    if [ ! -d "$DIR1" ] && [ -z "$( ls -A '$DIR1' )" ]; then
-        cd $DBDIR 
-        # Download the Linefinder input files from the database, check for bad files
-        if [ $ORACLE_DB="TRUE" ]; then
-            echo "Using Oracle db"
-            jid1=$(/bin/bash $FLASHDB/slurm_run_oracle_db_download.sh $SBID1 $DIR1 $BAD_FILES_DIR $FLASHPASS)
-        else
-            echo "Using Nimbus db"
-            jid1=$(/bin/bash $FLASHDB/slurm_run_db_download.sh $SBID1 $DIR1 $BAD_FILES_DIR $FLASHPASS)
-        fi
-        # Report
-        j1=$(echo $jid1 | awk '{print $4}')
-        echo "Sumbitted download job $j1"
-        cd $DETECTDIR
-        if [ "$MODE" != "INVERT" ]; then 
-            cp slurm_linefinder.ini model.txt $PARENT1/config
-            jid2=$(sbatch --dependency=afterok:$j1 $FINDER/slurm_run_flashfinder.sh $PARENT1 spectra_ascii $BAD_FILES_DIR $SBID1)
-        j2=$(echo $jid2 | awk '{print $4}')
-        echo "Sumbitted dependent detection job $j2"
-        echo "$j2 = sbid $SBID1" >> $DETECTDIR/jobs_to_sbids.txt
-    else
-        cd $DETECTDIR
-        cp slurm_linefinder.ini model.txt $PARENT1/config
-        jid2=$(sbatch $FINDER/slurm_run_flashfinder.sh $PARENT1 spectra_ascii $BAD_FILES_DIR $SBID1)
-        j2=$(echo $jid2 | awk '{print $4}')
-        echo "Sumbitted detection job $j2"
-        echo "$j2 = sbid $SBID1" >> $DETECTDIR/jobs_to_sbids.txt
-    fi
+    # Get the ASCII data from the client:
+    scp flash@$CLIENT:~/tmp/$SBID1/$SBID1*.tar.gz $DIR1/
+    # Delete ASCII directory on client:
+    ssh flash@$CLIENT "cd ~/tmp; rm -R $SBID1"
+    # Untar ASCII tarball
+    cd $DIR1; tar -zxvf $SBID*.tar.gz;rm $SBID*.tar.gz
+
+    # Process the files
+    cd $DETECTDIR
+    cp slurm_linefinder.ini model.txt $PARENT1/config
+    jid2=$(sbatch $FINDER/slurm_run_flashfinder.sh $PARENT1 spectra_ascii $BAD_FILES_DIR $SBID1)
+    j2=$(echo $jid2 | awk '{print $4}')
+    echo "Sumbitted detection job $j2"
+    echo "$j2 = sbid $SBID1" >> $DETECTDIR/jobs_to_sbids.txt
+
+    # Tar up results and send them back to the client for upload to the FLASH db
     cd $CRONDIR
     jid3=$(sbatch --dependency=afterok:$j2 tar_detection_outputs.sh $SBID)
     j3=$(echo $jid3 | awk '{print $4}')
