0da026b7922a8f78fde74199d47c4e7196109392
Gordon German <gordon.german@csiro.au>
2025-07-01 13:02:01 +0800

    copy sbid data to HPC

--- pipeline/cronjobs/client_platform/oracle_casda_update.sh
+++ pipeline/cronjobs/client_platform/oracle_casda_update.sh
@@ -7,7 +7,7 @@ CASDA_EMAIL="user@email"
 CASDA_PWD="password_at_CASDA"
 HPC_PLATFORM="setonix.pawsey.org.au"
 HPC_USER="user_at_hpc"
-
+HPC_SCRATCH="/scratch/ja3/$HPC_USER/data/casda"
 #######################################################################################################
 
 IFS=","
@@ -33,6 +33,21 @@ else
     echo "Starting spectral processing of new SBIDS at $HPC_PLATFORM"
 fi
 
-scp $CRONDIR/new_sbids.log $HPC_USER@$HPC_PLATFORM:~/src/cronjobs 
+scp $CRONDIR/new_sbids.log $HPC_USER@$HPC_PLATFORM:~/src/cronjobs
+
+# Copy across the SBID data to HPC
+sbids=$(sed "s/ //g" <<< $sbids)
+sbids="${sbids//\'}"
+SBIDS=$(sed "s/,/ /g" <<< $sbids)
+IFS=" "
+SBIDARRAY=()
+read -a SBIDARRAY <<< "$SBIDS"
+echo "downloaded: $SBIDS"
+for i in "${!SBIDARRAY[@]}"; do
+    SBID="${SBIDARRAY[$i]}"
+    scp -r $DATA/$SBID $HPC_USER@$HPC_PLATFORM:$HPC_SCRATCH/
+done
+
+# Trigger spectral processing on HPC
 ssh $HPC_USER@$HPC_PLATFORM "cd ~/src/cronjobs; ./casda_download_and_spectral.sh &> spectral.log;" 
 scp $HPC_USER@$HPC_PLATFORM:~/src/cronjobs/spectral.log /home/flash/src/cronjobs/
