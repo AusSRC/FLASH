808b3073ce6226238ad99ea814e91bef8fc8929f
Gordon German <gordon.german@csiro.au>
2025-07-01 12:45:48 +0800

    error in CATDIR default

--- database/db_utils.py
+++ database/db_utils.py
@@ -71,7 +71,7 @@ def set_parser():
             default="5432",
             help='database host port (default: %(default)s)')    
     parser.add_argument('-d', '--sbid_dir',
-            default="/scratch/ja3/ger063/data/casda",
+            default=CATDIR
             help='Specify local directory to use (default: %(default)s)')   
     parser.add_argument('-i', '--invert',
             default=False,
