9c45501b678f8c1469c31d2aaf008e60d607b229
Gordon German <gordon.german@csiro.au>
2025-07-25 14:52:26 +0800

    Use variables for client, key etc

--- pipeline/cronjobs/hpc_platform/push_spectral_to_oracle.sh
+++ pipeline/cronjobs/hpc_platform/push_spectral_to_oracle.sh
@@ -5,32 +5,40 @@
 # database. WARNING - it will delete any previous data for this 
 # SBID.
 #########################################################
-# Set the tmp dir on the client
+# Set the client platform details:
 TMPDIR=/mnt/tmp
-# Set the data dir (parent to SBID dir) on the client
 PARENTDIR=/mnt/casda
+CLIENT="152.67.97.254"
+ORACLE_KEY="~/.ssh/oracle_flash_vm.key"
+FLASHPASS="aussrc"
+#########################################################
+# $@ is the sbid(s) to process
+SBIDARRAY=( "$@" )
 
-# $1 is the sbid
-#
-QUALITY=`cat "$DATA"/"$1"/data_quality.txt`
-if [ -z "$QUALITY" ]; then
-    QUALITY="NOT_VALIDATED"
-fi
- 
-echo "Uploading $1 spectral plot results via Oracle to database"
+# $SBID is the sbid
+for SBID in "${SBIDARRAY[@]}"; do
 
-ssh -i ~/.ssh/oracle_flash_vm.key flash@152.67.97.254 "cd $PARENTDIR; rm -R $1/spectra_ascii* $1/spectra_plots* $TMPDIR/$1* $1/logs; mkdir -p $1/config $1/spectra_plots $1/spectra_ascii $1/SourceSpectra $1/logs;"
-scp -i ~/.ssh/oracle_flash_vm.key $DATA/$1/*ascii_tarball.tar.gz flash@152.67.97.254:$TMPDIR
-scp -i ~/.ssh/oracle_flash_vm.key $DATA/$1/*plots_tarball.tar.gz flash@152.67.97.254:$PARENTDIR/$1/spectra_plots/
-scp -i ~/.ssh/oracle_flash_vm.key $DATA/$1/config/* flash@152.67.97.254:$PARENTDIR/$1/config/
-scp -i ~/.ssh/oracle_flash_vm.key $DATA/$1/logs/plot_err*.log flash@152.67.97.254:$PARENTDIR/$1/logs/err.log
-scp -i ~/.ssh/oracle_flash_vm.key $DATA/$1/logs/plot_out*.log flash@152.67.97.254:$PARENTDIR/$1/logs/out.log
-scp -i ~/.ssh/oracle_flash_vm.key $DATA/catalogues/*$1*.xml flash@152.67.97.254:$PARENTDIR/catalogues/
-scp -i ~/.ssh/oracle_flash_vm.key $DATA/$1/*Spectra-image*.tar flash@152.67.97.254:$PARENTDIR/$1/
-scp -i ~/.ssh/oracle_flash_vm.key $DATA/$1/*sources_tarball.tar.gz flash@152.67.97.254:$PARENTDIR/$1/SourceSpectra/
-ssh -i ~/.ssh/oracle_flash_vm.key flash@152.67.97.254 "cd $PARENTDIR/$1/spectra_plots; tar -zxvf *plots_tarball.tar.gz; rm *plots_tarball.tar.gz"
-ssh -i ~/.ssh/oracle_flash_vm.key flash@152.67.97.254 "cd $PARENTDIR/$1/SourceSpectra; tar -zxvf *sources_tarball.tar.gz; rm *sources_tarball.tar.gz"
-ssh -i ~/.ssh/oracle_flash_vm.key flash@152.67.97.254 "source ~/set_local_flash_env.sh; cd ~/src/FLASH/database; python3 db_upload.py -m SPECTRAL -q $QUALITY -s $1 -t $TMPDIR -d $PARENTDIR -pw aussrc -cs config -C 'CASDA_upload'"
+    QUALITY=`cat "$DATA"/"$SBID"/data_quality.txt`
+    if [ -z "$QUALITY" ]; then
+        QUALITY="NOT_VALIDATED"
+    fi
+     
+    echo "Uploading $SBID spectral plot results via Oracle to database"
+
+    ssh -i ~/.ssh/oracle_flash_vm.key flash@$CLIENT "cd $PARENTDIR; rm -R $SBID/spectra_ascii* $SBID/spectra_plots* $TMPDIR/$SBID* $SBID/logs; mkdir -p $SBID/config $SBID/spectra_plots $SBID/spectra_ascii $SBID/SourceSpectra $SBID/logs;"
+    scp -i $ORACLE_KEY $DATA/$SBID/*ascii_tarball.tar.gz flash@$CLIENT:$TMPDIR
+    scp -i $ORACLE_KEY $DATA/$SBID/*plots_tarball.tar.gz flash@$CLIENT:$PARENTDIR/$SBID/spectra_plots/
+    scp -i $ORACLE_KEY $DATA/$SBID/config/* flash@$CLIENT:$PARENTDIR/$SBID/config/
+    scp -i $ORACLE_KEY $DATA/$SBID/logs/plot_err*.log flash@$CLIENT:$PARENTDIR/$SBID/logs/err.log
+    scp -i $ORACLE_KEY $DATA/$SBID/logs/plot_out*.log flash@$CLIENT:$PARENTDIR/$SBID/logs/out.log
+    scp -i $ORACLE_KEY $DATA/catalogues/*$SBID*.xml flash@$CLIENT:$PARENTDIR/catalogues/
+    scp -i $ORACLE_KEY $DATA/$SBID/*Spectra-image*.tar flash@$CLIENT:$PARENTDIR/$SBID/
+    scp -i $ORACLE_KEY $DATA/$SBID/*sources_tarball.tar.gz flash@$CLIENT:$PARENTDIR/$SBID/SourceSpectra/
+    ssh -i $ORACLE_KEY flash@$CLIENT "cd $PARENTDIR/$SBID/spectra_plots; tar -zxvf *plots_tarball.tar.gz; rm *plots_tarball.tar.gz"
+    ssh -i ~/.ssh/$ORACLE_KEY flash@$CLIENT "cd $PARENTDIR/$SBID/SourceSpectra; tar -zxvf *sources_tarball.tar.gz; rm *sources_tarball.tar.gz"
+    ssh -i ~/.ssh/$ORACLE_KEY flash@$CLIENT "source ~/set_local_flash_env.sh; cd ~/src/FLASH/database; python3 db_upload.py -m SPECTRAL -q $QUALITY -s $SBID -t $TMPDIR -d $PARENTDIR -pw $FLASHPASS -cs config -C 'CASDA_upload'"
+
+done
 
 # Stash the SLURM logs
 mv slurm-*.out $DATA/tmp/
