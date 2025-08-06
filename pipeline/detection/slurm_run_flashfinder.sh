4b44960c8f15c6f6fa131467cc8b81c9436b300f
Gordon German <gordon.german@csiro.au>
2025-08-06 10:14:49 +0800

    Updated modules for setonix

--- pipeline/detection/slurm_run_flashfinder.sh
+++ pipeline/detection/slurm_run_flashfinder.sh
@@ -16,13 +16,13 @@ mkdir -p "$1"/logs
 cd "$1"
 
 module load python/3.11.6
-module load py-numpy/1.24.4
-module load py-matplotlib/3.8.1
+module load py-numpy/1.25.2
+module load py-matplotlib/3.8.4
 module load py-astropy/5.1
 module load py-mpi4py/3.1.5-py3.11.6
 
 module load gcc/12.2.0
-module load py-scipy/1.11.3
+module load py-scipy/1.13.0
 
 
 export MPICH_GNI_MALLOC_FALLBACK=enabled
@@ -32,22 +32,22 @@ export FI_CXI_DEFAULT_VNI="$(od -vAn -N4 -tu < /dev/urandom)"
 export SLURM_EXPORT_ENV=ALL
 
 echo "Checking for bad files"
-python $FINDER/pre_process.py $3 $1/$2
+python3 $FINDER/pre_process.py $3 $1/$2
 
 
 echo "Starting with $1/$2"
 ## Ensure the correct linefinder.ini is specified here:
 if [ "$MODE" = "STD" ]; then
     mkdir -p "$1"/outputs
-    srun -K1 python $FINDER/flash_finder.py --data_path $1/$2 --model_path $1/config/model.txt --out_path $1/outputs \
+    srun -K1 python3 $FINDER/flash_finder.py --data_path $1/$2 --model_path $1/config/model.txt --out_path $1/outputs \
 --mask_path $1/config/mask.txt --sbid $4 --inifile $1/config/slurm_linefinder.ini
 elif [ "$MODE" = "INVERT" ]; then
     mkdir -p "$1"/inverted_outputs
-    srun -K1 python $FINDER/flash_finder.py --data_path $1/$2 --model_path $1/config/model.txt --out_path $1/inverted_outputs \
+    srun -K1 python3 $FINDER/flash_finder.py --data_path $1/$2 --model_path $1/config/model.txt --out_path $1/inverted_outputs \
 --mask_path $1/config/mask.txt --sbid $4 --inifile $1/config/slurm_linefinder_inverted.ini
 elif [ "$MODE" = "MASK" ]; then
     mkdir -p "$1"/masked_outputs
-    srun -K1 python $FINDER/flash_finder.py --data_path $1/$2 --model_path $1/config/model.txt --out_path $1/masked_outputs \
+    srun -K1 python3 $FINDER/flash_finder.py --data_path $1/$2 --model_path $1/config/model.txt --out_path $1/masked_outputs \
 --mask_path $1/config/mask.txt --sbid $4 --inifile $1/config/slurm_linefinder.ini
 fi
 
