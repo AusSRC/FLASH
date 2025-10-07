8cd9cba36cbfdbfd98b52cbecf746e357b06eead
Gordon German <gordon.german@csiro.au>
2025-10-07 10:18:10 +0800

    Can merge an old json list with new data

--- pipeline/detection/process_bad_file_dir.py
+++ pipeline/detection/process_bad_file_dir.py
@@ -1,8 +1,16 @@
 #########################################################################################
+#   Use this script to create a json file of the listed 'bad_ascii_files' kept in the bad_ascii_file directory:
+#           python3 process_bad_file_dir.py <bad_ascii directory>
 #
-#       This script will check the files stored in the BAD_ASCII_FILES directory and 
-#       create a list of sbids and source names that were tagged 'bad'. This list will be pushed to
-#       the Oracle FLASH VM periodically.
+#    The created json file will list files that meet the criteria of 'bad' - one of:
+#           1. More than 40% of flux values are NaN
+#           2. More than 40% of noise values are NaN
+#           3. Malformed lines of data
+#
+#   Optionally, you can merge an older json file with the new data:
+#           python3 process_bad_file_dir.py <bad_ascii directory> <previous bad_ascii json file>
+#
+#   The json file should be pushed to the Oracle FLASH VM periodically.
 #
 #      
 #       GWHG @ AusSRC 2025
@@ -15,7 +23,14 @@ import json
 import glob
 
 def usage():
-    print("python3 process_bad_file_dir.py <directory of bad files>")
+    print("\nUse this script to create a json file of the listed 'bad_ascii_files' kept in the bad_ascii_file directory:")
+    print("        python3 process_bad_file_dir.py <bad_ascii directory>")
+    print("\n This file will list files that meet the criteria of 'bad' - one of:")
+    print("        1. More than 40% of flux values are NaN")
+    print("        2. More than 40% of noise values are NaN")
+    print("        3. Malformed lines of data")
+    print("\nOptionally, you can merge an older json file with the new data:")
+    print("        python3 process_bad_file_dir.py <bad_ascii directory> <previous bad_ascii json file>")
 
 def getSubDirs(path):
     subdirectories = [entry.name for entry in os.scandir(path) if entry.is_dir()]
@@ -31,6 +46,33 @@ def getSbidsSourcesFromName(pathname):
     sbid = bits[0].split('SB')[1]
     source = bits[2]
     return sbid,source
+
+def readOldJson(fname):
+    with open(fname, 'r') as file:
+        data = json.load(file)
+
+    return data
+
+def _appendData(dat,old_data,new_data):
+    for line in new_data[dat]:
+        # Check if sbid in old data
+        if line in old_data[dat]:
+            for val in old_data[dat][line]:
+                if val not in new_data[dat][line]:
+                    new_data[dat][line].append(val)
+    return new_data
+
+def appendToJson(old_data,new_data):
+
+    new_vals = {}
+    # Flux data first
+    new_data = _appendData("flux",old_data,new_data)
+    # malformed data
+    new_data = _appendData("malformed",old_data,new_data)
+    # noise data
+    new_data = _appendData("noise",old_data,new_data)
+
+    return new_data
 
 ##########################################################################################
 ##########################################################################################
@@ -57,7 +99,16 @@ if __name__ == "__main__":
         vals[sub] = dict(sorted(vals[sub].items()))
         for key in vals[sub].items():
             print(f"{key}")
+    
+    print("Got new data")
 
+    # read the old bad_ascii file, if provided:
+    if len(sys.argv) > 2:
+        old_data = readOldJson(sys.argv[2])
+        # Merge the new and old data
+        print("Got old data: merging")
+        vals = appendToJson(old_data,vals)
+    
     # Write to file
     with open("bad_files.json", "w") as file:
         json.dump(vals, file, indent=4)
