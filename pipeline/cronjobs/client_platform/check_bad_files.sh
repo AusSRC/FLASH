0637b6616d4a0bc11473d688e6d075eaa69ce628
Gordon German <gordon.german@csiro.au>
2025-10-09 11:08:00 +0800

    Script to generate bad ascii report from HPC data

---
+++ pipeline/cronjobs/client_platform/check_bad_files.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+#######################################################################################################
+source $HOME/set_local_flash_env.sh
+
+echo "Checking for bad ascii files on HPC"
+
+# copy current bad_files.json to the HPC for merging with new data
+scp $HOME/src/cronjobs/bad_files/bad_files.json $HPC_USER@$HPC_PLATFORM:~/src/linefinder/current_bad_files.json
+
+# Run the HPC script to create new json file
+ssh $HPC_USER@$HPC_PLATFORM "cd ~/src/linefinder; python3 process_bad_file_dir.py $HPC_SCRATCH/bad_ascii_files current_bad_files.json"
+
+# Retrieve the new json file from HPC
+scp $HPC_USER@$HPC_PLATFORM:~/src/linefinder/bad_files.json $HOME/src/cronjobs/bad_files/
+
+echo "Done!"
