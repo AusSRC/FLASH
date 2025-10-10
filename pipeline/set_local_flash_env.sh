9192ce5dd339e30be1c05cafd6f30e7e18c3b6d1
Gordon German <gordon.german@csiro.au>
2025-10-10 13:03:45 +0800

    Added HPCDATA as an alternative to HPC_SCRATCH

--- pipeline/set_local_flash_env.sh
+++ pipeline/set_local_flash_env.sh
@@ -1,10 +1,58 @@
 #!/bin/bash
 
+# User's CASDA creds
+CASDA_EMAIL="user@email"
+export CASDA_EMAIL
+CASDA_PWD="password_at_CASDA"
+export CASDA_PWD
+
+# Database details (usually accessed via client machine)
+FLASHPASS="aussrc"
+export FLASHPASS
+HOST="10.0.2.225"
+PORT="5432"
+
 # Set HPC path to FLASH code
 FLASHHOME=/software/projects/ja3/ger063/setonix/FLASH
+# This path is for the CLIENT platform
 #FLASHHOME=/home/flash/src/FLASH
 export FLASHHOME
 
+# Set data dir for this machine
+DATA=/mnt/db/data/casda
+export DATA
+
+# Tmp dir for this machine
+TMPDIR=/mnt/db/data/tmp
+export TMPDIR
+
+# Set path to cron scripts
+CRONDIR=$HOME/src/cronjobs
+export CRONDIR
+
+# Client machine paths (eg Oracle VM)
+CLIENTTMP=/mnt/db/data/tmp
+export CLIENTTMP
+CLIENTDATA=/mnt/db/data/casda
+export CLIENTDATA
+CLIENTIP="152.67.97.254"
+export CLIENTIP
+CLIENTKEY="~/.ssh/oracle_flash_vm.key"
+export CLIENTKEY
+
+# Other HPC machine paths (eg setonix)
+HPC_PLATFORM="setonix.pawsey.org.au"
+export HPC_PLATFORM
+HPC_USER="user_at_hpc"
+export HPC_USER
+HPC_SCRATCH="/scratch/ja3/$HPC_USER/data/casda"
+export HPC_SCRATCH
+HPCDATA=$HPC_SCRATCH
+export HPCDATA
+
+# Blacklisted SBIDS directory
+BLACKLIST_DIRS=$DATA/blacklist_sbids
+export BLACKLIST_DIRS
 # Set HPC path to spectral plotting software and container
 SPECTRAL=$FLASHHOME/spectral_plot
 export SPECTRAL
@@ -23,52 +71,6 @@ export MULTINEST=$FLASHHOME/multinest
 # Set HPC path to database scripts
 FLASHDB=$FLASHHOME/database
 export FLASHDB
-
-# Set HPC path to cron scripts
-CRONDIR=$HOME/src/cronjobs
-export CRONDIR
-
-# User's CASDA creds
-CASDA_EMAIL="user@email"
-export CASDA_EMAIL
-CASDA_PWD="password_at_CASDA"
-export CASDA_PWD
-
-# Client machine paths (eg Oracle VM)
-CLIENTTMP=/mnt/db/data/tmp
-export CLIENTTMP
-CLIENTDATA=/mnt/db/data/casda
-export CLIENTDATA
-CLIENTIP="152.67.97.254"
-export CLIENTIP
-CLIENTKEY="~/.ssh/oracle_flash_vm.key"
-export CLIENTKEY
-
-# Database details (usually accessed via client machine)
-FLASHPASS="aussrc"
-export FLASHPASS
-HOST="10.0.2.225"
-PORT="5432"
-
-# Other HPC machine paths (eg setonix)
-HPC_PLATFORM="setonix.pawsey.org.au"
-export HPC_PLATFORM
-HPC_USER="user_at_hpc"
-export HPC_USER
-HPC_SCRATCH="/scratch/ja3/$HPC_USER/data/casda"
-export HPC_SCRATCH
-
-# Set data dir for this machine
-DATA=/mnt/db/data/casda
-export DATA
-
-# Tmp dir for this machine
-TMPDIR=/mnt/db/data/tmp
-export TMPDIR
-
-# Blacklisted SBIDS directory
-BLACKLIST_DIRS=$DATA/blacklist_sbids
-export BLACKLIST_DIRS
 
 # Add MultiNest library to dynamic library path
 export DYLD_LIBRARY_PATH=$MULTINEST/lib:${DYLD_LIBRARY_PATH}
