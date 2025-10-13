7147e3f67260ee91ffefd49743989a998f55ed17
Gordon German <gordon.german@csiro.au>
2025-10-13 11:22:35 +0800

    Add sbid to sbatch jobname

--- pipeline/spectral/run_container_spectral.sh
+++ pipeline/spectral/run_container_spectral.sh
@@ -12,7 +12,7 @@
 mkdir -p $1/$2/logs
 sbatch <<EOT
 #!/bin/bash
-#SBATCH --job-name=spectral_plot
+#SBATCH --job-name=plot_"$1"
 #SBATCH --output="$1"/"$2"/logs/plot_out.log
 #SBATCH --error="$1"/"$2"/logs/plot_err.log
 #SBATCH --time=02:00:00
