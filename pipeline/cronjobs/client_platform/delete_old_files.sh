522d2f0bc8a2d0b9b3bdfe3e49b54ab06403c211
riniangreani <rini.angreani@curtin.edu.au>
2025-11-19 22:52:17 +0800

    Fixed the script to get it to work

--- pipeline/cronjobs/client_platform/delete_old_files.sh
+++ pipeline/cronjobs/client_platform/delete_old_files.sh
@@ -4,36 +4,18 @@
 # m h  dom mon dow   command
 # 00 00 * * 1-5 /bin/bash /home/flash/src/cronjobs/oracle_casda_update.sh
 
-BASE_DIR="$(dirname "$0")"
-TMP_ROOT=$(realpath "$BASE_DIR/db_query/static/db_query")
-EXPIRE_SECONDS=86400
-NOW=$(date +%s)
-
-cleanup_folder() {
-    local folder=$1
-    if [ -d "$folder" ]; then
-        MOD_TIME=$(stat -c %Y "$folder")
-        AGE=$(( NOW - MOD_TIME ))
-
-        if [ $AGE -gt $EXPIRE_SECONDS ]; then
-            echo "Deleting old session folder: $folder"
-            rm -rf "$folder"
-        fi
-    fi
-}
+BASE_DIR="$HOME/src/FLASH/django/flashdb"
+TMP_ROOT="$BASE_DIR/db_query/static/db_query"
 
 # List of specific subfolders to clean
 SUBFOLDERS=("plots" "linefinder" "linefinder/masks" "ascii")
 echo "Starting cleanup of session folders older than 1 day..."
+
 for sub in "${SUBFOLDERS[@]}"; do
     TARGET="$TMP_ROOT/$sub"
-    if [ -d "$TARGET" ]; then
-        # Loop through session ID folders inside this subfolder
-        for session_folder in "$TARGET"/*; do
-            if [ -d "$session_folder" ]; then
-                cleanup_folder "$session_folder"
-            fi
-        done
-    fi
+    # delete all folders older than 1 day
+    # take care not to delete "masks" subfolder in linefinder
+    find "$TARGET" -mindepth 1 -type d -not -name "masks" -mtime +1 -exec rm -rf {} +
 done
+
 echo "Cleanup for old sessions complete."
