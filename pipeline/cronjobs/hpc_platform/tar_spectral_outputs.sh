18e8a8c0dc8b393a1caa72a4a9257f0a3c8df962
Gordon German <gordon.german@csiro.au>
2025-12-19 09:18:42 +0800

    Script will exit if tarball is not valid

--- pipeline/cronjobs/hpc_platform/tar_spectral_outputs.sh
+++ pipeline/cronjobs/hpc_platform/tar_spectral_outputs.sh
@@ -6,12 +6,31 @@
 # $@ is the sbid(s) to process
 SBIDARRAY=( "$@" )
 
+
 for SBID1 in "${SBIDARRAY[@]}"; do
 
     echo "Tarring $SBID1 spectral plot results"
 
     cd $DATA/$SBID1/spectra_ascii; tar -zcvf "$SBID1"_ascii_tarball.tar.gz *; mv "$SBID1"_ascii_tarball.tar.gz ../
+    tar -tf ../"$SBID1"_ascii_tarball.tar.gz
+    if [ $? -ne 0 ]; then
+        echo "$SBID1 ascii tarball invalid - exiting with error"
+        echo "$SBID1 ascii tarball invalid - exiting with error" >> $CRONDIR/tar_error.txt
+        exit 1
+    fi
     cd $DATA/$SBID1/spectra_plots; tar -zcvf "$SBID1"_plots_tarball.tar.gz *; mv "$SBID1"_plots_tarball.tar.gz ../
+    tar -tf ../"$SBID1"_plots_tarball.tar.gz
+    if [ $? -ne 0 ]; then
+        echo "$SBID1 plots tarball invalid - exiting with error"
+        echo "$SBID1 plots tarball invalid - exiting with error" >> $CRONDIR/tar_error.txt
+        exit 1
+    fi
     cd $DATA/$SBID1/SourceSpectra; tar -zcvf "$SBID1"_sources_tarball.tar.gz *; mv "$SBID1"_sources_tarball.tar.gz ../
+    tar -tf ../"$SBID1"_sources_tarball.tar.gz
+    if [ $? -ne 0 ]; then
+        echo "$SBID1 sources tarball invalid - exiting with error"
+        echo "$SBID1 sources tarball invalid - exiting with error" >> $CRONDIR/tar_error.txt
+        exit 1
+    fi
 done
 exit 0
