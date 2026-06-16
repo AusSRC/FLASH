dbd46f48c3dfae2158244581eddf30d416a781f1
Glen Rees <ree13d@csiro.au>
2026-06-16 16:35:09 +1000

    Attempting to fix the staticfiles issue and nginx blocking needed css/js

--- ansible/roles/ssh/files/deploy.sh
+++ ansible/roles/ssh/files/deploy.sh
@@ -19,8 +19,18 @@ fi
 
 export FLASH_IMAGE_TAG="$TAG"
 
+# Pull the docker image
 docker compose pull
 
+# Migrate database (once we start having it managed by django)
 #docker compose run --rm web python manage.py migrate
 
+# Tidy up any old staticfiles and media files
+rm -rf /srv/flashdb/static/*
+rm -rf /srv/flashdb/media/*
+
+# Create new static files using the docker container
+docker compose run --rm web python manage.py collectstatic --noinput
+
+# Start up the nginx and django services and clear up old ones
 docker compose up -d --remove-orphans
